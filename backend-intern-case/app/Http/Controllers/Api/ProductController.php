5a1108057b21c1945cd98bf90ea213fd9e9398d0
Raeaw <raffi.darrell.f@gmail.com>
2026-07-20 19:23:44 +0700

    feat: add API resource

---
+++ backend-intern-case/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::latest()->paginate(10);
+
+        return ProductResource::collection($products);
+    }
+
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+
+    public function store(StoreProductRequest $request)
+    {
+        $product = Product::create($request->validated());
+
+        return (new ProductResource($product))
+            ->response()
+            ->setStatusCode(201);
+    }
+
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        $product->update($request->validated());
+
+        return new ProductResource($product);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response()->json([
+            'data' => [
+                'message' => 'Product deleted successfully.',
+            ],
+        ]);
+    }
+}
