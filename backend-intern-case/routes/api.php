c25f221f4213a34012dd528b3b8dbce68b02c46e
Raeaw <raffi.darrell.f@gmail.com>
2026-07-20 18:34:10 +0700

    feat: add endpoint for testing middleware

--- backend-intern-case/routes/api.php
+++ backend-intern-case/routes/api.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum', 'admin'])->get('/test-admin', function () {
+    return response()->json(['data' => ['message' => 'Kamu admin!']]);
+});
