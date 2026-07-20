37bfe090d7f10d49beda802f36956dc457233fa9
Raeaw <raffi.darrell.f@gmail.com>
2026-07-20 19:52:15 +0700

    feat: request form for order

--- backend-intern-case/database/seeders/DatabaseSeeder.php
+++ backend-intern-case/database/seeders/DatabaseSeeder.php
@@ -2,24 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@example.com',
-            'password' => 'password', // plain text, cast akan otomatis hash
+            'password' => 'password',
             'role' => 'admin',
         ]);
 
@@ -29,5 +23,7 @@ class DatabaseSeeder extends Seeder
             'password' => 'password',
             'role' => 'user',
         ]);
+
+        Product::factory()->count(10)->create();
     }
 }
--- backend-intern-case/routes/api.php
+++ backend-intern-case/routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\AuthController;
 
 // Public
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{product}', [ProductController::class, 'show']);
 
