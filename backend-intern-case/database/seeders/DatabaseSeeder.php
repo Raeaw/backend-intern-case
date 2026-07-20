bc2f7312c5a8602209483e898141e32a4a408b82
Raeaw <raffi.darrell.f@gmail.com>
2026-07-20 18:23:12 +0700

    fix: seeder: user password plaint text, hash on cast

--- backend-intern-case/database/seeders/DatabaseSeeder.php
+++ backend-intern-case/database/seeders/DatabaseSeeder.php
@@ -16,10 +16,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => 'password', // plain text, cast akan otomatis hash
+            'role' => 'admin',
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'User Biasa',
+            'email' => 'user@example.com',
+            'password' => 'password',
+            'role' => 'user',
         ]);
     }
 }
