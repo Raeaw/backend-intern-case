f1f6b401d0de21c8b806267689517e8b589221c3
Raeaw <raffi.darrell.f@gmail.com>
2026-07-20 16:34:35 +0700

    feat: add model + migration: orderItem

--- backend-intern-case/app/Models/Order.php
+++ backend-intern-case/app/Models/Order.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-// app/Models/Order.php
 class Order extends Model
 {
     use HasFactory;
