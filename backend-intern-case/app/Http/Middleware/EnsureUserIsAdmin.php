dae937c0ef949aa02c1080c0af57650845e8ac47
Raeaw <raffi.darrell.f@gmail.com>
2026-07-20 21:46:48 +0700

    feat: response middleware

--- backend-intern-case/app/Http/Middleware/EnsureUserIsAdmin.php
+++ backend-intern-case/app/Http/Middleware/EnsureUserIsAdmin.php
@@ -8,13 +8,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class EnsureUserIsAdmin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  Closure(Request): (Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
+        if (! $request->user() || ! $request->user()->isAdmin()) {
+            return response()->json([
+                'message' => 'Forbidden. Admin access required.',
+            ], 403);
+        }
+
         return $next($request);
     }
 }
